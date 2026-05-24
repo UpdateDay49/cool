4f35ddb1927f26d04ad72135f921d16e877f2bf3
UpdateDay49 <115380745+UpdateDay49@users.noreply.github.com>
2026-05-24 09:49:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'h.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server running at ${port}`);
+    console.log(`Server running on port ${port}`);
 });
+
